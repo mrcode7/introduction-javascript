0db47cd8620af3d0c7877db9134c8f573eff9ecc
mrcode7 <learningmrcode@gmail.com>
2023-02-14 05:01:31 +0700

    variable dan type data

--- variableBiodata.js
+++ variableBiodata.js
@@ -1,45 +1,75 @@
-
 // Buat variabel dengan nama biodata dan tipe data object dengan value dan tipe data
 
-// String
-let name = "Hasbi Alwi Kusmana";
+// Object
+const biodata = {
+  // String
+  name: "John Doe",
 
-// Number
-let age = 22;
+  // Number
+  age: 20,
 
-// Array
-let hobbies = ["Membaca", "Bermain", "Belajar"];
+  // Array
+  hobbies: ["Playing Game", "Listening Music", "Watching Movie"],
 
-// Boolean
-let isMarried = false;
+  // Boolean
+  isMarried: false,
 
-// Array of Object
-let schoolList = [{
-    name: "SMKN 2 Ciamis",
-    yearIn: 2015,
-    yearOut: 2018,
-    major: "Teknik Kendaraan Ringan Otomotif"
-},
-{
-    name: "Universitas Pendidikan Indonesia",
-    yearIn: 2018,
-    yearOut: 2022,
-    major: "Teknik Informatika"
-}];
+  // Array of object
+  schoolList: [
+    {
+      name: "SDN 1 Malang",
 
-// Array of Object
-let skills = [{
-    skillName: "HTML",
-    level: "beginner"
-},
-{
-    skillName: "CSS",
-    level: "beginner"
-},
-{
-    skillName: "JavaScript",
-    level: "beginner"
-}];
+      yearIn: 2005,
 
-// Boolean
-let interestInCoding = true;
+      yearOut: 2011,
+
+      major: null,
+    },
+
+    {
+      name: "SMPN 1 Malang",
+
+      yearIn: 2011,
+
+      yearOut: 2014,
+
+      major: null,
+    },
+
+    {
+      name: "SMAN 1 Malang",
+
+      yearIn: 2014,
+
+      yearOut: 2017,
+
+      major: "IPA",
+    },
+  ],
+
+  skills: [
+    {
+      skillName: "HTML",
+
+      level: "beginner",
+    },
+
+    {
+      skillName: "CSS",
+
+      level: "beginner",
+    },
+
+    {
+      skillName: "Javascript",
+
+      level: "beginner",
+    },
+  ],
+
+  // Boolean
+  interestInCoding: true,
+};
+
+// Cetak variabel biodata
+console.log(biodata);
