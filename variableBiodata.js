65080b03f9a44d19c32446275d38898b4ddb57cf
mrcode7 <learningmrcode@gmail.com>
2023-02-14 04:51:23 +0700

    Introduction javascript variable

---
+++ variableBiodata.js
@@ -0,0 +1,45 @@
+
+// Buat variabel dengan nama biodata dan tipe data object dengan value dan tipe data
+
+// String
+let name = "Hasbi Alwi Kusmana";
+
+// Number
+let age = 22;
+
+// Array
+let hobbies = ["Membaca", "Bermain", "Belajar"];
+
+// Boolean
+let isMarried = false;
+
+// Array of Object
+let schoolList = [{
+    name: "SMKN 2 Ciamis",
+    yearIn: 2015,
+    yearOut: 2018,
+    major: "Teknik Kendaraan Ringan Otomotif"
+},
+{
+    name: "Universitas Pendidikan Indonesia",
+    yearIn: 2018,
+    yearOut: 2022,
+    major: "Teknik Informatika"
+}];
+
+// Array of Object
+let skills = [{
+    skillName: "HTML",
+    level: "beginner"
+},
+{
+    skillName: "CSS",
+    level: "beginner"
+},
+{
+    skillName: "JavaScript",
+    level: "beginner"
+}];
+
+// Boolean
+let interestInCoding = true;
